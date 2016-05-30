d0660b28fc7c12ac992bb371587ef5fee05b3ec8
Arshad <arshad4000@yahoo.co.in>
2016-05-30 23:57:26 +0530

    FirstCommit - Android Training with fragments

---
+++ app/src/main/java/andtraining/com/androidtrainingwithfragments/MainActivity.java
@@ -0,0 +1,49 @@
+package andtraining.com.androidtrainingwithfragments;
+
+import android.app.FragmentManager;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Load the inital activity into fragment container
+        if(findViewById(R.id.MyFragmentContainer)!=null) {
+            LoginFragment default_login_frag = new LoginFragment();
+            default_login_frag.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction().add(R.id.MyFragmentContainer,default_login_frag).commit();
+        }
+
+        onSwitchListeners();
+    }
+
+    public void onSwitchListeners() {
+        Switch switch_widget = (Switch)findViewById(R.id.switch1);
+        switch_widget.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {  //Load signup fragment
+                    SignUpFragment signup_frag = new SignUpFragment();
+                    FragmentTransaction txn = getSupportFragmentManager().beginTransaction();
+                    txn.replace(R.id.MyFragmentContainer, signup_frag);
+                    //txn.addToBackStack(null);
+                    txn.commit();
+                } else {  //Load login fragment
+                    LoginFragment login_frag = new LoginFragment();
+                    FragmentTransaction txn = getSupportFragmentManager().beginTransaction();
+                    txn.replace(R.id.MyFragmentContainer,login_frag);
+                    //txn.addToBackStack(null);
+                    txn.commit();
+                }
+            }
+        });
+    }
+}
